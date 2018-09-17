1a699e0fdc8053d332d048ff08c93137b8ff606a
Patawee <maronmate@users.noreply.github.com>
2018-09-17 14:29:22 +0700

    Add Component of priority and add axios

---
+++ Web/src/components/Priority/PriorityList.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <template>
+             <b-table striped hover :items="PriorityListItem" :fields="TableHeader">
+                <template slot="action" slot-scope="row">
+                    <b-button class="btn-edit mr-1" size="sm" @click.stop="deletePriority(row.index)">Edit</b-button>
+                    <b-button class="btn-delete mr-1" size="sm" @click.stop="deletePriority(row.index)">Remove</b-button>
+                </template>
+            </b-table>
+        </template>
+    </div>
+</template>
+<script>
+export default {
+
+    Name:'priority-list',
+    props: ['PriorityListItem'],
+    data: function () {
+        return {
+            TableHeader: [ 'priorityId','priorityNumber', 'color','action' ],
+        };
+    },
+    methods:
+    {
+            deletePriority(index)
+            {
+
+            }
+    }
+}
+</script>
--- Web/src/views/ManagePriority.vue
+++ Web/src/views/ManagePriority.vue
@@ -1,18 +1,77 @@
 <template>
     <div>
-          <nav-layout>
+        <nav-layout>
             <h1>Manage Priority</h1>
+            <div class="container">
+                <div class="form-group">
+                    <div class="row">
+                        <div class="col-md-4 col-md-offset-4">	
+					        <label class="col-sm-4 control-label">Priority Number: </label>
+                        </div>
+					    <div class="col-sm-8">
+						    <input v-model="PriorityNumber" type="number" style="width: 300px;">
+                        </div>
+					</div>
+                    <div class="row">  
+                            <div class="col-md-4 col-md-offset-4">
+                                <label class="col-sm-4 control-label">Color: </label>
+                            </div>
+                            <div class="col-sm-8">
+                                <color-picker-chrome :color="Color" v-model="Color" />
+					        </div>				            
+                    </div>
+                </div>  
+            </div>
+            <div class="table-priority"> 
+                <priority-list v-bind:PriorityListItem="PriorityListItem"></priority-list>
+            </div>
         </nav-layout>
     </div>
     
 </template>
 <script>
-export default {
-        name: 'ManagePriority',
+import PriorityList from '@/components/Priority/PriorityList'
+import ColorPickerChrome from '@/components/Priority/ColorPickerChrome'
+import axios from 'axios';
 
-        mounted() {
-    console.log(this.$route.path)
-    this.$emit('page-change', this.$route.name)
-  }
+export default {
+    name: 'ManagePriority',
+    components: {
+        PriorityList,
+        ColorPickerChrome,
+    },
+
+    mounted() {
+        console.log(this.$route.path)
+        this.$emit('page-change', this.$route.name)
+        this.loadPriority();
+    },
+    data: function () {
+        return {
+            PriorityId: null,
+            PriorityNumber: null,
+            Color:null,
+            PriorityListItem: [],
+            ApiHost: 'https://localhost:44382',
+        };
+    },
+    beforeMount(){
+        
+    },
+    
+    methods:
+    {
+        loadPriority()
+        {
+            const url = this.ApiHost +'/api/Priorities';
+            var vm = this;
+             axios.get(url)
+                .then(function (response) {
+                    console.log(response.data);
+                    vm.PriorityListItem = response.data;
+                });
+        }
+    }
+
 }
 </script>
