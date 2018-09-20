f2b119b5777e1da326901c693e3d4f3cc0fa473b
BunditSpace <spidar21@gmail.com>
2018-09-20 11:48:04 +0700

    fix ManageType Modal

--- Web/src/components/Type/TypeList.vue
+++ Web/src/components/Type/TypeList.vue
@@ -12,15 +12,15 @@
                  
                 <template slot="action" slot-scope="row">
                     <b-button v-show="!showEdit" variant="success sm" class="btn-edit mr-1" size="sm" @click.stop="$emit('editType', row.item)">Edit</b-button>
-                    <b-button variant="danger sm" class="btn-delete mr-1" size="sm" v-b-modal="`modal${row.item.typeId}`">Remove</b-button>
-                    <b-modal :id="`modal${row.item.typeId}`"
-                        title="Confirm Remove"
-                        @ok="$emit('deleteType', row.item)">
-                            Do you sure to Remove Type "{{row.item.name}}" ?
-                    </b-modal>
+                    <b-button variant="danger sm" class="btn-delete mr-1" size="sm" v-b-modal.typeModal @click="OpenTypeModal(row.item)">Remove</b-button>
                 </template>
             </b-table>
         </template>
+        <b-modal id="typeModal"
+            title="Confirm Remove"
+            @ok="confirmDelete">
+                {{ deleteMessage }}
+        </b-modal>
     </div>
 </template>
 
@@ -32,8 +32,20 @@ export default {
             fileds:[
                 {  key: 'name',   label: 'Type Name' },
                 {  key: 'action', lable: ' Action' }
-            ]
+            ],
+            deleteType: null,
+            deleteMessage: ''
         }
+     },
+     methods:{
+         OpenTypeModal(type){
+            this.deleteType = type;
+            this.deleteMessage = "Do you sure to Remove Type " + this.deleteType.name
+         },
+         confirmDelete(){
+             if(this.deleteType != null)
+                this.$emit('deleteType', this.deleteType)
+         }
      }
 }
 </script>
